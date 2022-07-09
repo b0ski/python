adef3031bb912fdef43ce227e1d3eea3e3596a1e
roman shuralov <guy.lebowski@gmail.com>
2022-07-09 15:07:57 +0300

    max num (list method)

--- app/lists/max_num.py
+++ app/lists/max_num.py
@@ -4,7 +4,7 @@ def max_num(param):
             if instance_l1 < instance_l2:
                 maximum = instance_l2
     print(maximum, param.index(maximum))
-    print(max(param), param.index(max(param)))
+#   print(max(param), param.index(max(param)))
 
 max_num((1, 666662, 43, 2345, 3245, 4232))
 
