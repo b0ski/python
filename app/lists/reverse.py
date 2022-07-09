6e7245a3a29032f183e40b05f8ec0e55b3fe6fc4
roman shuralov <guy.lebowski@gmail.com>
2022-07-09 15:14:42 +0300

    reverse

--- app/lists/reverse.py
+++ app/lists/reverse.py
@@ -1,9 +1,6 @@
 def reverse(param):
-    for instance_l1 in param:
-        for instance_l2 in param:
-            if instance_l1 < instance_l2:
-                maximum = instance_l2
-    print(maximum, param.index(maximum))
+    param.reverse()
+    print(param)
 
 
-reverse((1, 666662, 43, 2345, 3245, 4232))
+reverse([1, 666662, 43, 2345, 3245, 4232])
