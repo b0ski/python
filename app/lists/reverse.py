ebc8b86e569abd3faf9b2c9423d16f5a457c2b28
roman shuralov <guy.lebowski@gmail.com>
2022-07-09 15:07:07 +0300

    max num (list method)

--- app/lists/reverse.py
+++ app/lists/reverse.py
@@ -1,10 +1,9 @@
-def max_num(param):
+def reverse(param):
     for instance_l1 in param:
         for instance_l2 in param:
             if instance_l1 < instance_l2:
                 maximum = instance_l2
     print(maximum, param.index(maximum))
-    print(max(param), param.index(max(param)))
 
-max_num((1, 666662, 43, 2345, 3245, 4232))
 
+reverse((1, 666662, 43, 2345, 3245, 4232))
