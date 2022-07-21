41488bdc7b725d7d8091c267996c32de5bc10bab
roman shuralov <guy.lebowski@gmail.com>
2022-07-21 13:54:49 +0300

    first half of eshop

--- app/classes/eshop.py
+++ app/classes/eshop.py
@@ -13,19 +13,52 @@ class Category:
 
 
 class Basket:
+    item = None
+
     def __init__(self, items: list):
         self.items = items
 
-    def add(self):
-        print('added')
+    def add(self, item):
+        self.items.append(item)
+        print(f'{item} added to the basket')
 
-    def delete(self):
-        print('deleted')
+    def delete(self, item):
+        self.items.remove(item)
+        print(f'{item} removed from the basket')
+
+    def checkout(self, item, user):
+        if item in self.items:
+            if user.money >= item.price:
+                self.items.remove(item)
+                user.money = user.money - item.price
+                print(f'You bought {item.name}')
+
+        else:
+            print("you don't have enough money")
 
 
 class User:
-    def __init__(self, login: str, pss: str, basket: Basket):
+    def __init__(self, login: str, pss: str, money: int, basket: Basket):
         self.login = login
         self.pss = pss
+        self.money = money
         self.basket = basket
+
+
+item_1 = Item('iphone', 999, 4, 45)
+item_2 = Item('samsung', 980, 5, 87)
+item_3 = Item('xiaomi', 500, 4, 120)
+item_4 = Item('sony', 770, 3, 89)
+item_5 = Item('oppo', 120, 3, 12)
+
+item_list = [item_1, item_2, item_3, item_4, item_5]
+
+user_1 = User('Peter', 'qwerty', 5, Basket(item_list))
+user_2 = User('Lisa', '12345', 2000, Basket(item_list))
+user_3 = User('Elizabeth', 'adfLJK&-3w', 1000, Basket(item_list))
+
+user_list = [user_1, user_2, user_3]
+
+user_2.basket.checkout(item_1, user_2)
+
 
